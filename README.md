d5d481144ccc5485f72fa660300a0d44f33d3425
Jeppe <jepperm@gmail.com>
2014-05-01 18:29:03 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,125 @@
+LevelAPI
+==============
+This library will make it possible to inject your userscript into the page and still have access to functions like GM_setValue and GM_getValue.
+
+How does it work?
+-----------------
+LevelAPI sets up a message system between the page and the userscript. This system
+works by sending stringified objects by using `JSON`. In these objects there are
+information on which function to call and with what arguments. LevelAPI will when
+sending arguments transform them into a `safe` object where the functions are
+stored into the memory and an unique id is generated which will be sent instead
+of the function. The reciever will then be able to identify from where that
+unique function id came from and how to handle it.
+
+All this is made into a simple API where it's not needed to know how the system
+works and how the transformation works.
+
+Requirements
+------------
+This library is supported on all browsers which supports `postMessage` and the
+event `message`.
+
+Getting started
+---------------
+To start using this library you need to include `levelAPI.js` in your userscript.
+After that you will have to move your userscript into a function which you can
+pass to `scopeScript`. You might also need to change on which object LevelAPI
+will be accessible from. This can be done by changing `window` at the bottom
+of `levelAPI.js` to another variable of your choosing.
+
+API
+---
+The API of LevelAPI is made into two parts. One for the userscript and one for
+the injected script/function.
+
+### Injecting the script (Only for the Userscript)
+To inject your script into the page you will have to make a function which you can
+pass to `scopeScript`:
+```JavaScript
+function exampleScript(levelAPI) {
+  // ... your code
+}
+```
+
+This function can then be injected into the page by calling
+```JavaScript
+scopeScript(exampleScript);
+```
+
+### Setting the functions
+This function will link the functions you want to be made available for the injected
+script or userscript (depends on where you call the function).
+
+To set the functions you call `setFunctions` with an object as the argument. Every
+entry in the object is each one callable function. The object key is the name of
+the function and the value is the function itself. An example of this can be
+```JavaScript
+{
+  "GM_getValue": GM_getValue,
+  "GM_setValue": GM_setValue
+}
+```
+
+So to link `GM_getValue` and `GM_setValue` to LevelAPI you call `setFunctions`
+```JavaScript
+setFunctions({
+  "GM_getValue": GM_getValue,
+  "GM_setValue": GM_setValue
+});
+```
+
+### Calling functions
+To call a function can only be done to the functions made available through the
+function `setFunctions`.
+
+The function `call` can have up to 3 arguments where only the 2 first are required.
+The first argument is the name of the function you want to call. The second argument
+is the arguments you want to give the function when you call it. This argument needs
+to be in an array. If you don't want to give any arguments just give it an empty
+array. The last argument is the callback function which will be called when the
+function you called has been executed.
+
+An example of `call`
+```JavaScript
+call("GM_getValue", ["myKey"], function(value){
+  // ... my callback code
+});
+```
+
+In the example the function `GM_getValue` is called with the argument `myKey`. As soon
+as this function has been completed it will call the callback function. In this case
+the callback function will be given value as an argument, because the function
+`GM_getValue` returns the value of the given key.
+
+The `call` function is run asynchronous and therefore call will return `null` no matter
+what function you try to call.
+
+TODO
+----
+* Create a garbage collector which will clean callback functions which have already been
+used.
+* Create an API to delete stored functions.
+
+License
+-------
+The MIT License (MIT)
+
+Copyright (c) 2014 Jeppe Rune Mortensen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ levelAPI.js
@@ -0,0 +1,344 @@
+/**
+  The MIT License (MIT)
+
+  Copyright (c) 2014 Jeppe Rune Mortensen
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ **/
+(function(win){
+  "use strict";
+  
+  function injectedScript() {
+    function isArray(arr) {
+      return Object.prototype.toString.call(arr) === "[object Array]";
+    }
+    function createSafeObject(obj) {
+      if (isArray(obj)) {
+        var i;
+        for (i = 0; i < obj.length; i++) {
+          obj[i] = createSafeObject(obj[i]);
+        }
+      } else if (typeof obj === "function") {
+        return createFunctionLink(obj);
+      } else if (obj === Object(obj)) {
+        var key;
+        for (key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            obj[key] = createSafeObject(obj[key]);
+          }
+        }
+      }
+      return obj;
+    }
+    function restoreOriginalObject(obj) {
+      if (isArray(obj)) {
+        var i;
+        for (i = 0; i < obj.length; i++) {
+          obj[i] = restoreOriginalObject(obj[i]);
+        }
+      } else if (typeof obj === "string" && isFunctionLink(obj)) {
+        return getFunctionByLink(obj);
+      } else if (obj === Object(obj)) {
+        var key;
+        for (key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            obj[key] = restoreOriginalObject(obj[key]);
+          }
+        }
+      }
+      return obj;
+    }
+    
+    function bind(func, b){
+      return func.call.apply(func.bind, arguments);
+    }
+    
+    function createFunctionCaller(funcLink) {
+      return bind(function(){ call(funcLink, Array.prototype.slice.call(arguments)); });
+    }
+    
+    function isFunctionLink(funcLink) {
+      return !!/^#\/func\/(safe|unsafe)\/id=([0-9]+);#$/g.exec(funcLink);
+    }
+    
+    function getFunctionByLink(funcLink) {
+      var exec = /^#\/func\/(safe|unsafe)\/id=([0-9]+);#$/g.exec(funcLink);
+      if(!exec) throw new Error("Malformed function link!");
+      
+      if (exec[1] === "unsafe") {
+        if (functionStorage.length <= exec[2]) throw new Error("Function not found in memory!");
+        console.log("Getting function with id=" + exec[2], functionStorage, functionStorage[exec[2]]);
+        return functionStorage[exec[2]];
+      } else if (exec[1] === "safe") {
+        var func = createFunctionCaller(funcLink);
+        callbackFunctionStorage[funcLink] = func;
+        return func;
+      }
+      
+      return functionStorage[exec[2]];
+    }
+    function getFunctionLinkInCallerStorage(func) {
+      var key;
+      for (key in callbackFunctionStorage) {
+        if (callbackFunctionStorage.hasOwnProperty(key) && callbackFunctionStorage[key] === func) {
+          return key;
+        }
+      }
+      
+      return null;
+    }
+    function createFunctionLink(func) {
+      var funcLink = getFunctionLinkInCallerStorage(func);
+      if (funcLink) {
+        return funcLink;
+      } else if (typeof func === "function") {
+        var id = functionStorage.push(func) - 1;
+        return "#/func/unsafe/id=" + id + ";#";
+      } else {
+        return null;
+      }
+    }
+    function safeWindowMessageListener(e) {
+      if (!e || !e.data) return; // Checking if data is present
+      if (typeof e.data !== "string") return; // Checking if the object is a string.
+      if (!e.data.indexOf || e.data.indexOf("{") !== 0) return;
+      
+      var d = JSON.parse(e.data);
+      if (d.address !== "safeLevel") return;
+      
+      if (d.method) {
+        if (typeof unsafeFunctions[d.method] === "function") {
+          var args = restoreOriginalObject(d.arguments);
+          var returnVal = unsafeFunctions[d.method].apply(window, args);
+          if (d.callback) {
+            call(d.callback, [returnVal]);
+          }
+        } else {
+          throw new Error("Unknown method!");
+        }
+      } else if (d.call) {
+        var args = restoreOriginalObject(d.arguments);
+        var func = getFunctionByLink(d.call);
+        
+        var returnVal = func.apply(window, args);
+        if (d.callback) {
+          call(d.callback, [returnVal]);
+        }
+      }
+    }
+    function call(_call, args) {
+      window.postMessage(JSON.stringify({
+        address: "pageLevel",
+        call: _call,
+        arguments: createSafeObject(args)
+      }), "*");
+    }
+    function callMethod(method, args, callback) {
+      window.postMessage(JSON.stringify({
+        address: "pageLevel",
+        method: method,
+        arguments: createSafeObject(args),
+        callback: createFunctionLink(callback)
+      }), "*");
+    }
+    function initPageLevel() {
+      window.addEventListener("message", safeWindowMessageListener, false);
+    }
+    function setFunctions(unsafeFuncs) {
+      unsafeFunctions = unsafeFuncs
+    }
+    var functionStorage = [], callbackFunctionStorage = {}, unsafeFunctions = {};
+    
+    initPageLevel();
+    
+    return { setFunctions: setFunctions, call: callMethod };
+  }
+  function isArray(arr) {
+    return Object.prototype.toString.call(arr) === "[object Array]";
+  }
+  function stringifyArguments(args) {
+    if (!isArray(args)) return null;
+    var str = JSON.stringify(args);
+    
+    return str.substring(1, str.length - 1);
+  }
+  function injectScript(func, filename, args) {
+    try {
+      var script = document.createElement("script"), p = (document.body || document.head || document.documentElement);
+      if (!p) return;
+      script.setAttribute("type", "text/javascript");
+      
+      var sourceURL = (typeof filename === "string" ? "\n//# sourceURL=" + encodeURIComponent(filename) : "");
+      var argsStringified = stringifyArguments(args) || "";
+      
+      script.appendChild(document.createTextNode("(" + func + ")(" + argsStringified + ");" + sourceURL));
+      p.appendChild(script);
+      p.removeChild(script);
+    } catch (e) {}
+  }
+  function createSafeObject(obj) {
+    if (isArray(obj)) {
+      var i;
+      for (i = 0; i < obj.length; i++) {
+        obj[i] = createSafeObject(obj[i]);
+      }
+    } else if (typeof obj === "function") {
+      return createFunctionLink(obj);
+    } else if (obj === Object(obj)) {
+      var key;
+      for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          obj[key] = createSafeObject(obj[key]);
+        }
+      }
+    }
+    return obj;
+  }
+  function restoreOriginalObject(obj) {
+    if (isArray(obj)) {
+      var i;
+      for (i = 0; i < obj.length; i++) {
+        obj[i] = restoreOriginalObject(obj[i]);
+      }
+    } else if (typeof obj === "string" && isFunctionLink(obj)) {
+      return getFunctionByLink(obj);
+    } else if (obj === Object(obj)) {
+      var key;
+      for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          obj[key] = restoreOriginalObject(obj[key]);
+        }
+      }
+    }
+    return obj;
+  }
+  
+  function bind(func, b){
+    return func.call.apply(func.bind, arguments);
+  }
+  
+  function createFunctionCaller(funcLink) {
+    return bind(function(){ call(funcLink, Array.prototype.slice.call(arguments)); });
+  }
+  
+  function isFunctionLink(funcLink) {
+    return !!/^#\/func\/(safe|unsafe)\/id=([0-9]+);#$/g.exec(funcLink);
+  }
+  
+  function getFunctionByLink(funcLink) {
+    var exec = /^#\/func\/(safe|unsafe)\/id=([0-9]+);#$/g.exec(funcLink);
+    if(!exec) throw new Error("Malformed function link!");
+    
+    if (exec[1] === "safe") {
+      if (functionStorage.length <= exec[2]) throw new Error("Function not found in memory!");
+      console.log("Getting function with id=" + exec[2], functionStorage, functionStorage[exec[2]]);
+      return functionStorage[exec[2]];
+    } else if (exec[1] === "unsafe") {
+      var func = createFunctionCaller(funcLink);
+      callbackFunctionStorage[funcLink] = func;
+      return func;
+    }
+    
+    return functionStorage[exec[2]];
+  }
+  function getFunctionLinkInCallerStorage(func) {
+    var key;
+    for (key in callbackFunctionStorage) {
+      if (callbackFunctionStorage.hasOwnProperty(key) && callbackFunctionStorage[key] === func) {
+        return key;
+      }
+    }
+    
+    return null;
+  }
+  function createFunctionLink(func) {
+    var funcLink = getFunctionLinkInCallerStorage(func);
+    if (funcLink) {
+      return funcLink;
+    } else if (typeof func === "function") {
+      var id = functionStorage.push(func) - 1;
+      return "#/func/safe/id=" + id + ";#";
+    } else {
+      return null;
+    }
+  }
+  function unsafeWindowMessageListener(e) {
+    if (!e || !e.data) return; // Checking if data is present
+    if (typeof e.data !== "string") return; // Checking if the object is a string.
+    if (!e.data.indexOf || e.data.indexOf("{") !== 0) return;
+    
+    var d = JSON.parse(e.data);
+    if (d.address !== "pageLevel") return
+    
+    if (d.method) {
+      if (typeof safeFunctions[d.method] === "function") {
+        var args = restoreOriginalObject(d.arguments);
+        var returnVal = safeFunctions[d.method].apply(window, args);
+        if (d.callback) {
+          call(d.callback, [returnVal]);
+        }
+      } else {
+        throw new Error("Unknown method!");
+      }
+    } else if (d.call) {
+      var args = restoreOriginalObject(d.arguments);
+      var func = getFunctionByLink(d.call);
+      
+      var returnVal = func.apply(window, args);
+      if (d.callback) {
+        call(d.callback, [returnVal]);
+      }
+    }
+  }
+  function call(_call, args) {
+    window.postMessage(JSON.stringify({
+      address: "safeLevel",
+      call: _call,
+      arguments: createSafeObject(args)
+    }), "*");
+  }
+  function callMethod(method, args, callback) {
+    window.postMessage(JSON.stringify({
+      address: "safeLevel",
+      method: method,
+      arguments: createSafeObject(args),
+      callback: createFunctionLink(callback)
+    }), "*");
+  }
+  function initPageLevel() {
+    window.addEventListener("message", unsafeWindowMessageListener, false);
+  }
+  function setFunctions(safeFuncs) {
+    safeFunctions = safeFuncs;
+  }
+  function scopeScript(func) {
+    var script = document.createElement("script"), p = (document.body || document.head || document.documentElement);
+    if (!p) return;
+    script.setAttribute("type", "text/javascript");
+    
+    script.appendChild(document.createTextNode("(function(){var levelAPI = (" + injectedScript + ")();(function(window, document, unsafeWindow){(" + func + ").apply(Object.create(null), [levelAPI]);}).apply(Object.create(null), [{}, document, window]);})();\n//# sourceURL=sandbox.js"));
+    p.appendChild(script);
+    p.removeChild(script);
+  }
+  var functionStorage = [], callbackFunctionStorage = {}, safeFunctions = {};
+  
+  // Initialize the connection
+  initPageLevel();
+  
+  win.levelAPI = { setFunctions: setFunctions, call: callMethod, scopeScript: scopeScript };
+})(window);
